360c5dc908f83041fdbc463b52b2f87666c54507
Bryan Wyatt <brwyatt@gmail.com>
2019-03-06 21:27:16 -0800

    Add setup.py and re-arrange some things

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/user/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='brwyatt_web',
+    version='0.1.0',
+    author='Bryan Wyatt',
+    author_email='brwyatt@gmail.com',
+    description=('Web code for brwyatt.net'),
+    license='GPLv3',
+    keywords='aws web website serverless lambda',
+    url='https://github.com/brwyatt/brwyatt.net',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=False,
+    entry_points={},
+    install_requires=[
+        'boto3',
+    ]
+)
