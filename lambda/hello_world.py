e2ce45a343d4df0ce1e851430202475694d3c179
Bryan Wyatt <brwyatt@gmail.com>
2019-03-06 22:31:15 -0800

    Return HTML explicitly from HelloWorld

--- lambda/hello_world.py
+++ lambda/hello_world.py
@@ -1,14 +1,17 @@
 def handler(event, context):
     return {
         'statusCode': '200',
-        'body': """
-        <html>
-        <head>
-          <title>Hello World!</title>
-        </head>
-        <body>
-          <h1>Hello World!</h1>
-        </body>
-        </html>
-        """
+        'headers': {
+            'Content-Type': 'text/html',
+        },
+        'body': (
+            '<html>'
+            '<head>'
+            '  <title>Hello World!</title>'
+            '</head>'
+            '<body>'
+            '  <h1>Hello World!</h1>'
+            '</body>'
+            '</html>'
+        )
     }
