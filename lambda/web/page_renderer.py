0877b262e8428769d63a5700f4ebc217433bafa8
Bryan Wyatt <brwyatt@gmail.com>
2019-03-07 23:54:44 -0800

    Add logging and error handling to lambda renderer

--- lambda/web/page_renderer.py
+++ lambda/web/page_renderer.py
@@ -1,10 +1,25 @@
+from brwyatt_web.logging import setup_logging
 from brwyatt_web.web import render_page
-from brwyatt_web.web.errors import error404
+from brwyatt_web.web.errors import error404, error500
 from brwyatt_web.web.routes import routes
 
 
+log = setup_logging()
+
+
 def handler(event, context):
+    log.info('Serving request from "{}" for "{}"'.format(
+        event['requestContext']['identity']['sourceIp'],
+        event['path']))
+    log.debug('event: {}'.format(event))
+
     if event['path'] in routes:
-        return render_page(**routes[event['path']]())
+        log.debug('Path found in route list, rendering.')
+        try:
+            return render_page(**routes[event['path']]())
+        except Exception as e:
+            log.critical('Unexpected exception rendering route "{}": {} - {}'
+                         .format(event['path'], e.__class__.__name__, str(e)))
+            return error500()
     else:
         return error404()
