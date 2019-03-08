8d26ab0e32d92888245dee87252a95b2aa8a1ca2
Bryan Wyatt <brwyatt@gmail.com>
2019-03-07 22:28:27 -0800

    Implement static content loader

--- lambda/web/fetch_static.py
+++ lambda/web/fetch_static.py
@@ -1,33 +1,50 @@
-import os
+from brwyatt_web.logging import setup_logging
+from brwyatt_web.file_loader import load_static_asset
+from brwyatt_web.file_loader import FileNotFoundException, PathSecurityException
+from brwyatt_web.web.errors import error400, error404, error500
+
+
+log = setup_logging()
 
 
 def handler(event, context):
-    return {
-        'statusCode': '200',
-        'headers': {
-            'Content-Type': 'text/html',
-        },
-        'body': (
-            '<html>'
-            '<head>'
-            '  <title>STATIC TEST</title>'
-            '</head>'
-            '<body>'
-            '  <h1>STATIC TEST</h1>'
-            '  <h2>path</h2>'
-            '  <pre>{path}</pre>'
-            '  <h2>resource</h2>'
-            '  <pre>{resource}</pre>'
-            '  <h2>css</h2>'
-            '  <pre>{css_dir}</pre>'
-            '  <h2>js</h2>'
-            '  <pre>{js_dir}</pre>'
-            '</body>'
-            '</html>'
-        ).format(
-            path=event['path'],
-            resource=event['resource'],
-            css_dir='\n'.join(os.listdir('/opt/static/css')),
-            js_dir='\n'.join(os.listdir('/opt/static/js'))
-        )
-    }
+    log.info('Serving request from "{}" for "{}"'.format(
+        event['requestContext']['identity']['sourceIp'],
+        event['path']))
+    log.debug('event: {}'.format(event))
+
+    if event['resource'].startswith('/css/'):
+        asset_type = 'css'
+        content_type = 'text/css'
+    elif event['resource'].startswith('/js/'):
+        asset_type = 'js'
+        content_type = 'application/javascript'
+    else:
+        log.error('Got unrecognized resource: "{}"'.format(event['resource']))
+        return error400()
+
+    file_name = event['pathParameters']['resource']
+
+    log.debug('Fetching "{}" resource with name "{}"'.format(
+        asset_type, file_name))
+
+    try:
+        return {
+            'statusCode': '200',
+            'headers': {
+                'Content-Type': content_type,
+            },
+            'body': load_static_asset(asset_type, file_name)
+        }
+    except FileNotFoundException as e:
+        log.error('Unable to find resource "{}/{}": {}'
+                  .format(asset_type, file_name, str(e)))
+        return error404()
+    except PathSecurityException as e:
+        log.error('Exception fetching resource "{}/{}": {}'
+                  .format(asset_type, file_name, str(e)))
+        return error400()
+    except Exception as e:
+        log.error('Unexpected exception fetching resource "{}/{}": {}'
+                  .format(asset_type, file_name, str(e)))
+        return error500()
---
+++ src/brwyatt_web/file_loader.py
@@ -0,0 +1,56 @@
+import logging
+import os
+
+
+log = logging.getLogger(__name__)
+
+
+class PathSecurityException(Exception):
+    pass
+
+
+class FileNotFoundException(Exception):
+    pass
+
+
+def load_static_asset(asset_type, file_name):
+    """
+    """
+    log.info('Loading static asset: {0}: {1}'.format(asset_type, file_name))
+
+    static_path = os.environ.get('STATIC_PATH',
+                                 os.path.join(os.getcwd(), 'static'))
+    log.debug('static_path = {}'.format(static_path))
+
+    asset_dir = os.path.abspath(os.path.join(static_path, asset_type))
+    log.debug('asset_dir = {}'.format(asset_dir))
+    if static_path != os.path.commonpath(static_path, asset_dir):
+        errmsg = 'Asset directory is outside static assets directory'
+        log.error(errmsg)
+        raise PathSecurityException(errmsg)
+    elif not os.pat.isdir(asset_dir):
+        errmsg = 'Asset type "{}" is invalid or does not exist'.format(
+            asset_type)
+        log.error(errmsg)
+        raise FileNotFoundException(errmsg)
+
+    file_path = os.path.abspath(os.path.join(asset_dir, file_name))
+    log.debug('asset file_path = {}'.format(file_path))
+    if asset_dir != os.path.commonpath(asset_dir, file_path):
+        errmsg = 'Requested asset is outside of expected asset directory'
+        log.error(errmsg)
+        raise PathSecurityException(errmsg)
+    elif not os.pat.isfile(file_path):
+        errmsg = 'Asset file could not be found'
+        log.error(errmsg)
+        raise FileNotFoundException(errmsg)
+
+    log.info('Asset size: {}'.format(os.path.getsize(file_path)))
+
+    with open(file_path, 'r') as asset:
+        content = asset.read()
+
+    log.info('Successfully fetched "{}" asset "{}"'.format(asset_type,
+                                                           file_name))
+
+    return content
