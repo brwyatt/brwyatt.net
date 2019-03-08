984b51dba58d6398226b99fab86da54f3b17f828
Bryan Wyatt <brwyatt@gmail.com>
2019-03-07 23:43:32 -0800

    Tweak page renderer and template

--- src/brwyatt_web/web/__init__.py
+++ src/brwyatt_web/web/__init__.py
@@ -1,7 +1,7 @@
 from jinja2 import Template
 
 
-def render_page(title, content, status=200):
+def render_page(title='Untitled', content='No Content', status=200):
     page_template = Template(
         '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"'
         '  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
@@ -10,7 +10,8 @@ def render_page(title, content, status=200):
         '    <title>brwyatt.net - {{title}}</title>'
         '  </head>'
         '  <body>'
-        '    <h1>{{title}}</h1>'
+        '    <h1>brwyatt.net</h1>'
+        '    <h2>{{title}}</h2>'
         '    <div>{{content}}</div>'
         '  </body>'
         '</html>'
