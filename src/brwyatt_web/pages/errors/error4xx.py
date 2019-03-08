29d4fad8676653db072b647336bbae95bd9fca59
Bryan Wyatt <brwyatt@gmail.com>
2019-03-08 12:08:56 -0800

    Fix return codes to strings

--- src/brwyatt_web/pages/errors/error4xx.py
+++ src/brwyatt_web/pages/errors/error4xx.py
@@ -9,12 +9,12 @@ log = logging.getLogger(__name__)
 def error400(errmsg='Client Error'):
     log.info('Rendering 400!')
     res = render_page('/errors/400', status_msg=errmsg)
-    res['statusCode'] = 400
+    res['statusCode'] = '400'
     return res
 
 
 def error404(errmsg='File not found'):
     log.info('Rendering 404!')
     res = render_page('/errors/404', status_msg=errmsg)
-    res['statusCode'] = 404
+    res['statusCode'] = '404'
     return res
--- src/brwyatt_web/pages/errors/error5xx.py
+++ src/brwyatt_web/pages/errors/error5xx.py
@@ -9,5 +9,5 @@ log = logging.getLogger(__name__)
 def error500(errmsg='Server Error'):
     log.info('Rendering 500!')
     res = render_page('/errors/500', status_msg=errmsg)
-    res['statusCode'] = 500
+    res['statusCode'] = '500'
     return res
