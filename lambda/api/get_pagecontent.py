85dadc7baed7aaddbafad18167edcb2d9c917923
Bryan Wyatt <brwyatt@gmail.com>
2019-03-09 15:20:47 -0800

    Add implementation to get_pagecontent handler
    Mostly the same as the page_renderer, but has to handle things a bit
    differently.

--- lambda/api/get_pagecontent.py
+++ lambda/api/get_pagecontent.py
@@ -1,11 +1,53 @@
 from datetime import datetime
 from json import dumps
 
+from brwyatt_web.logging import setup_logging
+
+
+log = setup_logging()
+
 
 def handler(event, context):
-    return {
-        'statusCode': '200',
-        'body': dumps({
-            'time': str(datetime.utcnow())
-        })
-    }
+    log.info('Serving request from "{}" for "{}"'.format(
+        event['requestContext']['identity']['sourceIp'],
+        event['path']))
+    log.debug('event: {}'.format(event))
+
+    queryParams = event['queryStringParameters'] or {}
+
+    if 'page' in queryParams:
+        try:
+            resp = render_page(queryParams['page'], format='json', event=event)
+        except FileNotFoundException as e:
+            log.error('Caught 404 while rendering "{}"'.format(
+                queryParams['page']))
+            resp = {
+                'statusCode': '404',
+                'body': json.dumps({
+                    'Error': '404',
+                    'Message': 'Not found'
+                })
+            }
+        except Exception as e:
+            log.critical('Unexpected exception rendering route "{}": {} - {}'
+                         .format(queryParams['page'], e.__class__.__name__,
+                                 str(e)))
+            resp = {
+                'statusCode': '500',
+                'body': json.dumps({
+                    'Error': '500',
+                    'Message': 'Server error'
+                })
+            }
+    else:
+        log.error('Request did not provide a page parameter')
+        resp = {
+            'statusCode': '400',
+            'body': json.dumps({
+                'Error': '400',
+                'Message': 'Missing "page" parameter'
+            })
+        }
+
+    log.debug(f'sending response to client:\n{resp}')
+    return resp
