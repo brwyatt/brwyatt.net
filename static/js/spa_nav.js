4b21b8410173e2730f31afe034a5b98455b56a14
Bryan Wyatt <brwyatt@gmail.com>
2019-03-10 00:32:54 -0800

    Code cleanup

--- static/js/spa_nav.js
+++ static/js/spa_nav.js
@@ -2,16 +2,24 @@ function spa_nav(destination) {
   $.ajax({
     url: api+"/pages/content?page="+destination,
     success: function(result) {
-      browser_title = "Bryan Wyatt - " + result.title
-      $("head > title").html(browser_title)
-      $("#sitecontent > #pagetitle > h2").html(result.title)
-      $("#sitecontent > #pagecontent").html(result.content)
-      window.history.pushState({}, browser_title, result.path)
-      ga('set', 'page', result.path)
-      ga('send', 'pageview')
-      $("#viewpane > #navbar > ul > li").removeClass("sel")
+      browser_title = "Bryan Wyatt - " + result.title;
+
+      // Set page content
+      $("head > title").html(browser_title);
+      $("#sitecontent > #pagetitle > h2").html(result.title);
+      $("#sitecontent > #pagecontent").html(result.content);
+
+      // Update browser history
+      window.history.pushState({}, browser_title, result.path);
+
+      // Log the action in Google Analytics
+      ga('set', 'page', result.path);
+      ga('send', 'pageview');
+
+      // Reset the navbar selections
+      $("#viewpane > #navbar > ul > li").removeClass("sel");
       // Will need to fix this to match what's done in the Jinja2 template
-      $("#viewpane > #navbar > ul > li > a[href='"+result.path+"']").parent().addClass("sel")
+      $("#viewpane > #navbar > ul > li > a[href='"+result.path+"']").parent().addClass("sel");
     },
     error: function(result) {
       console.log('ERROR');
