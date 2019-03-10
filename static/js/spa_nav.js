0892d595a5790ecf78607d3c31361cc174ac68a6
Bryan Wyatt <brwyatt@gmail.com>
2019-03-10 00:56:18 -0800

    Add popstate handler
    ... so now the back button works!

--- static/js/spa_nav.js
+++ static/js/spa_nav.js
@@ -1,31 +1,47 @@
-function spa_nav(destination) {
+// Add handler for the back button
+window.addEventListener('popstate', function(event) {
+  if(event.state) {
+    spa_nav(event.state.path, false)
+  }
+}, false);
+
+function spa_nav(destination, updateHistory=true) {
   $.ajax({
     url: api+"/pages/content?page="+destination,
     success: function(result) {
-      browser_title = "Bryan Wyatt - " + result.title;
+      update_page(result.path, result.title, result.content);
 
-      // Set page content
-      $("head > title").html(browser_title);
-      $("#sitecontent > #pagetitle > h2").html(result.title);
-      $("#sitecontent > #pagecontent").html(result.content);
-
-      // Update browser history
-      window.history.pushState({}, browser_title, result.path);
-
-      // Log the action in Google Analytics
-      ga('set', 'page', result.path);
-      ga('send', 'pageview');
-
-      // Reset the navbar selections
-      $("#viewpane > #navbar > ul > li").removeClass("sel");
-      // Will need to fix this to match what's done in the Jinja2 template
-      $("#viewpane > #navbar > ul > li > a[href='"+result.path+"']").parent().addClass("sel");
+      if(updateHistory) {
+        // Update browser history
+        window.history.pushState({
+          title: result.title,
+          path: result.path
+        }, browser_title, result.path);
+      }
     },
     error: function(result) {
       console.log('ERROR');
       console.log(result.status);
     }
   });
+}
+
+function update_page(path, title, content) {
+      browser_title = "Bryan Wyatt - " + title;
+
+      // Set page content
+      $("head > title").html(browser_title);
+      $("#sitecontent > #pagetitle > h2").html(title);
+      $("#sitecontent > #pagecontent").html(content);
+
+      // Log the action in Google Analytics
+      ga('set', 'page', path);
+      ga('send', 'pageview');
+
+      // Reset the navbar selections
+      $("#viewpane > #navbar > ul > li").removeClass("sel");
+      // Will need to fix this to match what's done in the Jinja2 template
+      $("#viewpane > #navbar > ul > li > a[href='"+path+"']").parent().addClass("sel");
 }
 
 /* vim: set ts=2 sw=2 sts=2 expandtab: */
