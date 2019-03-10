e2d7a6feefa43fbf718b975466f29b9c27c4e05c
Bryan Wyatt <brwyatt@gmail.com>
2019-03-10 00:22:42 -0800

    First stab at the SPA navigation

--- static/js/spa_nav.js
+++ static/js/spa_nav.js
@@ -1,5 +1,25 @@
 function spa_nav(destination) {
-  return true;
+  $.ajax({
+    url: api+"/pages/content?page="+destination,
+    success: function(result) {
+      browser_title = "Bryan Wyatt - " + result.title
+      $("head > title").html(browser_title)
+      $("#sitecontent > #pagetitle > h2").html(result.title)
+      $("#sitecontent > #pagecontent").html(result.content)
+      window.history.pushState({}, browser_title, result.path)
+      ga('set', 'page', result.path)
+      ga('send', 'pageview')
+      $("#viewpane > #navbar > ul > li").removeClass("sel")
+      // Will need to fix this to match what's done in the Jinja2 template
+      $("#viewpane > #navbar > ul > li > a[href='"+result.path+"']").parent().addClass("sel")
+    },
+    error: function(result) {
+      console.log('ERROR');
+      console.log(result.status);
+    }
+  });
+
+  return false; // Disable link following
 }
 
 /* vim: set ts=2 sw=2 sts=2 expandtab: */
