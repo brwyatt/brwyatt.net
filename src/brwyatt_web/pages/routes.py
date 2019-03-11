d66a004228fcc4800577f06d580218eebd9e4d46
Bryan Wyatt <brwyatt@gmail.com>
2019-03-10 20:57:47 -0700

    Move page content templates to their own subdir
    This will leave the top level for more cor page templates.

--- src/brwyatt_web/pages/routes.py
+++ src/brwyatt_web/pages/routes.py
@@ -1,8 +1,8 @@
 routes = {
-    '/': 'home.html',
-    '/about': 'about.html',
-    '/contact': 'contact.html',
-    '/projects': 'projects.html',
+    '/': 'pages/home.html',
+    '/about': 'pages/about.html',
+    '/contact': 'pages/contact.html',
+    '/projects': 'pages/projects.html',
     '/errors/400': 'errors/400.html',
     '/errors/404': 'errors/404.html',
     '/errors/500': 'errors/500.html',
