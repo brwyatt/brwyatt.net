75cc8c1faa703282edf6af233d82f53ec5c4b06a
Bryan Wyatt <brwyatt@gmail.com>
2019-03-12 21:43:13 -0700

    Allow error pages to render as JSON

--- src/brwyatt_web/pages/errors/error4xx.py
+++ src/brwyatt_web/pages/errors/error4xx.py
@@ -6,15 +6,17 @@ from brwyatt_web.pages import render_page
 log = logging.getLogger(__name__)
 
 
-def error400(errmsg='Client Error', event={}):
+def error400(errmsg='Client Error', format='html', event={}):
     log.info('Rendering 400!')
-    res = render_page('/errors/400', status_msg=errmsg, event=event)
+    res = render_page('/errors/400', format=format, status_msg=errmsg,
+                      event=event)
     res['statusCode'] = '400'
     return res
 
 
-def error404(errmsg='File not found', event={}):
+def error404(errmsg='File not found', format='html', event={}):
     log.info('Rendering 404!')
-    res = render_page('/errors/404', status_msg=errmsg, event=event)
+    res = render_page('/errors/404', format=format, status_msg=errmsg,
+                      event=event)
     res['statusCode'] = '404'
     return res
--- src/brwyatt_web/pages/errors/error5xx.py
+++ src/brwyatt_web/pages/errors/error5xx.py
@@ -6,8 +6,9 @@ from brwyatt_web.pages import render_page
 log = logging.getLogger(__name__)
 
 
-def error500(errmsg='Server Error', event={}):
+def error500(errmsg='Server Error', format='html', event={}):
     log.info('Rendering 500!')
-    res = render_page('/errors/500', status_msg=errmsg, event=event)
+    res = render_page('/errors/500', format=format, status_msg=errmsg,
+                      event=event)
     res['statusCode'] = '500'
     return res
