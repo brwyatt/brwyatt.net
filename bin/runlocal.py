cc7d572c0619687a2233295421ad3e7684b881e2
Bryan Wyatt <brwyatt@gmail.com>
2019-03-14 14:39:08 -0700

    Add routes/content for favicon and keybase proof

--- bin/runlocal.py
+++ bin/runlocal.py
@@ -39,6 +39,8 @@ routes = load_handlers([
     (r'^/$', 'lambda/web/page_renderer.py'),
     (r'^/css/(?P<resource>.*)$', 'lambda/web/fetch_static.py'),
     (r'^/js/(?P<resource>.*)$', 'lambda/web/fetch_static.py'),
+    (r'^/favicon.ico$', 'lambda/web/fetch_static.py'),
+    (r'^/keybase.txt$', 'lambda/web/fetch_static.py'),
     (r'^/pages/content$', 'lambda/api/get_pagecontent.py'),
     (r'^/(?P<proxy>.*)$', 'lambda/web/page_renderer.py'),
 ])
