191c450f763ddf4f9cf7f0495f86094c5e52a5f7
Bryan Wyatt <brwyatt@gmail.com>
2019-04-13 11:40:41 -0700

    Load routes from files
    This includes static routes (for runlocal.py) and dynamic routes
    (replaces the old route.py).

--- src/brwyatt_web/pages/routes.py
+++
@@ -1,9 +0,0 @@
-routes = {
-    '/': 'pages/home.html',
-    '/about': 'pages/about.html',
-    '/contact': 'pages/contact.html',
-    '/projects': 'pages/projects.html',
-    '/errors/400': 'errors/400.html',
-    '/errors/404': 'errors/404.html',
-    '/errors/500': 'errors/500.html',
-}
